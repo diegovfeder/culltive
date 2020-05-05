0f3ab2805549ade67bf742589d11e06aa3e7aa16
diegovfeder <diegovfeder@gmail.com>
2020-05-05 11:46:03 -0300

    add to package.json and styles.tsx

--- AppCulltive/src/Styles.tsx
+++ AppCulltive/src/Styles.tsx
@@ -11,6 +11,75 @@ import {
 
 export const someStyles = StyleSheet.create({
   primary: {
-    backgroundColor: '#3ea341',
+    color: '#3ea341',
+  },
+  secondary: {
+    color: '#3cbc40',
+  },
+  background: {
+    backgroundColor: '#f6f7f8',
+  },
+  button: {
+    marginVertical: 6,
+    display: 'flex',
+    height: 64,
+    borderRadius: 64 / 2,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#3cbc40',
+    shadowOpacity: 5,
+    shadowRadius: 20,
+    elevation: 10,
+    borderColor: '#707070',
+    borderWidth: 0.5,
+  },
+  h1: {
+    fontSize: 24,
+    fontFamily: 'Montserrat-Bold',
+    color: '#959595',
+    alignSelf: 'center',
+    margin: 8,
+  },
+  h2: {
+    fontSize: 20,
+    fontFamily: 'Montserrat-Bold',
+    color: '#959595',
+  },
+  h3: {
+    fontSize: 16,
+    fontFamily: 'Montserrat-Regular',
+    color: '#959595',
+  },
+  h4: {
+    fontSize: 14,
+    fontFamily: 'Montserrat-Bold',
+    color: '#959595',
+    marginVertical: 2,
+  },
+  modalView: {
+    backgroundColor: 'white',
+    margin: 16,
+    paddingHorizontal: 20,
+    paddingVertical: 16,
+    borderRadius: 20,
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 1,
+    shadowRadius: 5,
+    elevation: 5,
+  },
+  textButton: {
+    fontSize: 20,
+    fontFamily: 'Montserrat-Bold',
+    color: '#fff',
+  },
+  textLink: {
+    fontSize: 12,
+    fontFamily: 'Montserrat-Regular',
+    color: '#3cbc40',
+    textDecorationLine: 'underline',
   },
 });
