67e7c552e4d2ef3ded089fca7dd450dd8e24b66b
diegovfeder <diegovfeder@gmail.com>
2020-05-12 18:50:53 -0300

    fix (Android Device)

--- AppCulltive/index.js
+++ AppCulltive/index.js
@@ -1,23 +1,3 @@
-/**
- * Culltive React Native App
- * https://github.com/culltive/react-native-app
- *
- * Produced with:
-    TypeScript
-    React Native Libraries  -
-                            -
-                            -
-                            -
-
-    Firebase  - Firestore
-              - Cloud Functions
-              -
-
- *
- *
- *
- */
-
 import {AppRegistry} from 'react-native';
 import App from './src/App';
 import {name as appName} from './app.json';
---
+++ AppCulltive/readme.txt
@@ -0,0 +1,19 @@
+/**
+ * Culltive React Native App
+ * https://github.com/culltive/react-native-app
+ *
+ * Produced with:
+    TypeScript
+    React Native Libraries  -
+                            -
+                            -
+                            -
+
+    Firebase  - Firestore
+              - Cloud Functions
+              -
+
+ *
+ *
+ *
+ */
